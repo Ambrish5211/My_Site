88e8f090b231e30f67e885010f23d283d76b811a
Ambrish <ambrish19.5211@gmail.com>
2023-10-31 14:56:56 +0530

    fixing all posts and single post

--- Blog/views.py
+++ Blog/views.py
@@ -1,31 +1,26 @@
-from datetime import date
+from django.shortcuts import render, get_object_or_404
+
 from .models import Post
-
-from django.shortcuts import render
-
-
 
 # Create your views here.
 
-def get_date(post):
-  return post['date']
 
 def starting_page(request):
-    latest_posts=Post.objects.all().order_by("-date")[:3]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     return render(request, "blog/index.html", {
       "posts": latest_posts
     })
 
 
 def posts(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-posts.html", {
-      "all_posts": Post.objects.all()
+      "all_posts": all_posts
     })
 
 
-
 def post_detail(request, slug):
-    identified_post = next(post for post in Post.objects.all() if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
       "post": identified_post
     })
