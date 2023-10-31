ef3c6dbe82bf3ca7c3192200e0a0cb8649748939
Ambrish <ambrish19.5211@gmail.com>
2023-10-31 13:29:26 +0530

    added tag and author model

--- Blog/models.py
+++ Blog/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+class Tag(models.Model):
+  caption = models.CharField(max_length=20)
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email_address = models.EmailField()
+
 
 
 class Post(models.Models):
