4f0641926e064ab8b7360438e7b51df45187b3d9
Ambrish <ambrish19.5211@gmail.com>
2023-11-02 02:16:54 +0530

    post detail css modified

--- Blog/views.py
+++ Blog/views.py
@@ -22,5 +22,6 @@ def posts(request):
 def post_detail(request, slug):
     identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {
-      "post": identified_post
+      "post": identified_post,
+      "post_tags": identified_post.tags.all()
     })
