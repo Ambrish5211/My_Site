bdf20345f2e51139ae9d13bfbcd294d71e033f40
Ambrish <ambrish19.5211@gmail.com>
2023-11-04 01:26:04 +0530

    finished comment part

--- Blog/models.py
+++ Blog/models.py
@@ -3,18 +3,18 @@ from django.core.validators import MinLengthValidator
 
 # Create your models here.
 
-class Tag(models.Model):
-  caption = models.CharField(max_length=20)
 
-  def __str__(self):
-      return self.caption
+class Tag(models.Model):
+    caption = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.caption
 
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
-
 
     def full_name(self):
         return f"{self.first_name} {self.last_name}"
@@ -23,21 +23,24 @@ class Author(models.Model):
         return self.full_name()
 
 
-
 class Post(models.Model):
     title = models.CharField(max_length=150)
     excerpt = models.CharField(max_length=200)
-    image= models.ImageField(upload_to="posts", null=True)
+    image = models.ImageField(upload_to="posts", null=True)
     date = models.DateField(auto_now=True)
     slug = models.SlugField(unique=True, db_index=True)
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(
-        Author, on_delete=models.SET_NULL,null=True, related_name="posts")
+        Author, on_delete=models.SET_NULL, null=True, related_name="posts")
     tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
     user_name = models.CharField(max_length=120)
     user_email = models.EmailField() 
     text = models.TextField(max_length=400)
     post = models.ForeignKey(
-        Post, on_delete=models.CASCADE, related_name="comments")    
+        Post, on_delete=models.CASCADE, related_name="comments")
