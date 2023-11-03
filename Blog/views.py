0cddb2ae5da9d60bf663f64144174f4943a32a21
Ambrish <ambrish19.5211@gmail.com>
2023-11-04 02:59:16 +0530

    finishing backend(hopefully)

--- Blog/views.py
+++ Blog/views.py
@@ -9,6 +9,7 @@ from .forms import CommentForm
 
 # Create your views here.
 
+
 class StartingPageView(ListView):
     template_name = "blog/index.html"
     model = Post
@@ -29,13 +30,24 @@ class AllPostsView(ListView):
 
 
 class SinglePostView(View):
+    def is_stored_post(self, request, post_id):
+        stored_posts = request.session.get("stored_posts")
+        if stored_posts is not None:
+          is_saved_for_later = post_id in stored_posts
+        else:
+          is_saved_for_later = False
+
+        return is_saved_for_later
+
     def get(self, request, slug):
         post = Post.objects.get(slug=slug)
+        
         context = {
-          "post": post,
-          "post_tags": post.tags.all(),
-          "comment_form": CommentForm(),
-          "comments": post.comments.all().order_by("-id")
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
         }
         return render(request, "blog/post-detail.html", context)
 
@@ -44,20 +56,21 @@ class SinglePostView(View):
         post = Post.objects.get(slug=slug)
 
         if comment_form.is_valid():
-          comment = comment_form.save(commit=False)
-          comment.post = post
-          comment.save()
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
 
-          return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
 
         context = {
-          "post": post,
-          "post_tags": post.tags.all(),
-          "comment_form": comment_form,
-          "comments": post.comments.all().order_by("-id")
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": comment_form,
+            "comments": post.comments.all().order_by("-id"),
+            "saved_for_later": self.is_stored_post(request, post.id)
         }
         return render(request, "blog/post-detail.html", context)
-    
+
 
 class ReadLaterView(View):
     def get(self, request):
