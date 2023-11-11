38f908eae4acab5c1f807ccd1cbde5da580277ed
Ambrish <ambrish19.5211@gmail.com>
2023-11-12 01:55:04 +0530

    changed app name

--- My_Site/settings.py
+++ My_Site/settings.py
@@ -23,19 +23,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='django-insecure-wc64&njy+w6x5vt6c^^@7f&h_en2tp53qyvyi$eyby1%7e1+jk' # os.environ.get("SECRET_KEY")
-# 
+SECRET_KEY = os.environ.get("SECRET_KEY")
+# 'django-insecure-wc64&njy+w6x5vt6c^^@7f&h_en2tp53qyvyi$eyby1%7e1+jk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True # os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = [] # os.environ.get("ALLOWED_HOST")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOST").split(" ")
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'blog',
+    'Blog',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -89,7 +89,7 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-database_url = "postgres://blog_site_oi06_user:4jo2TMIRFlX2xiz3KDsveHy0PImyjOOa@dpg-cl7s9jiuuipc73ekur20-a.oregon-postgres.render.com/blog_site_oi06"
+database_url = os.environ.get("DATABASE_URL")
 DATABASES['default'] = dj_database_url.parse(database_url)
 
 
